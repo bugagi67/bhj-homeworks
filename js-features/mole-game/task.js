7100f006ca072eb2ed2c5864961b61c0cff4de4d
Dmitriy Greshnov <dmitrygreshnov2@gmail.com>
2022-09-13 21:47:03 +0300

    Task 1.3

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let dead = document.getElementById("dead");
+let lost = document.getElementById("lost");
+
+for (let index = 1; index <= 9; index++) {
+
+  function getHole(index) {
+    let holeCLick = document.getElementById(`hole${index}`);
+    return holeCLick;
+  }
+
+  let hole = getHole(index);
+
+  hole.onclick = function() {
+    if (hole.classList.contains( 'hole_has-mole' )) {
+      dead.innerHTML++;
+    } else {
+      lost.innerHTML++;
+    }
+
+    if (dead.textContent === '10') {
+      alert("Вы победили!");
+      dead.textContent = 0;
+      lost.textContent = 0;
+    } else if (lost.textContent === '5') {
+      alert("Вы проиграли...");
+      dead.textContent = 0;
+      lost.textContent = 0;
+    }
+  }
+}

0d18f6dbd1b50ddb03289e98eb33671005d11f4d
Dmitriy Greshnov <dmitrygreshnov2@gmail.com>
2022-09-23 00:18:11 +0300

    Task 1.2-1

---
+++ element-search/popups/task.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const modalMain = document.getElementById("modal_main");
+const modalSuccess = document.getElementById("modal_success");
+const closeModal = document.querySelectorAll(".modal__content div");
+const showSuccess = document.querySelector(".show-success");
+
+
+setTimeout(() => modalMain.classList.add("modal_active"), 1000);
+
+
+Array.from(closeModal).forEach( elem => {
+  elem.onclick = function() {
+    modalMain.classList.remove("modal_active");
+    modalSuccess.classList.remove("modal_active");
+  }
+})
+
+showSuccess.onclick = function () {
+  modalMain.classList.remove("modal_active");
+  modalSuccess.classList.add("modal_active");
+}
+
+
+

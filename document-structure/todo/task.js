f4c7fe18b24569062d9bd5e00442aca2367bc2b6
Dmitriy Greshnov <dmitrygreshnov2@gmail.com>
2022-10-27 01:15:44 +0300

    Task 2.3-2

---
+++ document-structure/todo/task.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const add = document.querySelector(".tasks__add");
+const input = document.querySelector(".tasks__input");
+const list = document.querySelector(".tasks__list");
+
+add.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (input.value.trim()) {
+    list.insertAdjacentHTML(
+      "afterbegin",
+      `<div class="task">
+          <div class="task__title">
+            ${input.value}
+          </div>
+          <a href="#" class="task__remove">&times;</a>
+      </div>`
+    );
+    input.value = "";
+
+    let task = document.querySelector(".task");
+    let remove = document.querySelector(".task__remove");
+
+    remove.addEventListener("click", () => task.remove());
+
+    localStorage.setItem("list", 'list');
+  }
+});
